a74ca7104d3ba9b136be7134a951c34c3a277d69
ualia946 <i946@inlumine.ual.es>
2025-08-15 15:23:45 +0200

    Cambio de nombre de ACR a un nombre global

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr-flask" {
-  name                = "acrflask"
+  name                = "acrflaskgitactions"
   resource_group_name = azurerm_resource_group.rg.name
   location            = "France Central"
   sku                 = "Basic"
