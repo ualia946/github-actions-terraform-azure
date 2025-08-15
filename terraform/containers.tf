b3829d22e1ec4de024de224b6e4996b86663486f
ualia946 <i946@inlumine.ual.es>
2025-08-15 15:40:08 +0200

    Nombre del contendor cambiado

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -22,7 +22,7 @@ resource "azurerm_container_group" "aci-flask" {
   }
 
   container {
-    name   = "dvwa"
+    name   = "flask-web"
     image  = "${azurerm_container_registry.acr-flask.login_server}/flask-app:${var.docker_image_tag}"
     cpu    = "1.0"
     memory = "1.0"
